5afe31237e08272504d65918693a9853fbefc700
sanjay <sanjayprashanth1997@gmail.com>
2023-04-19 10:32:44 +0530

    react-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const data = [
+    {
+      image : "https://i.pinimg.com/originals/f4/ed/18/f4ed18e4f8c07a1688153de9daa5bd6c.jpg",
+      name : "Jerry",
+      batch : "B45WD"
+    },
+    {
+      image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUHTnj8JJzOluz-hGYk_Xtamzb_ALB5e3hlzuvDzYGkcVaA2yBBHx1pG2Elcd-drYE9x8&usqp=CAU",
+      name : "Tom",
+      batch : "B45WD"
+    },
+    {
+      image : "https://m.media-amazon.com/images/I/416K4jaHdbL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg",
+      name : "Bheem",
+      batch : "B45WD"
+    }
+  ]
+
+// data.map((value, index, accArr) => console.log(value, index, accArr))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+     data.map((profile, index)=>(
+      <Profile
+      key = {index}
+      image={profile.image}
+      name = {profile.name}
+      batch={profile.batch}
+      />
+     ))
+     }
     </div>
   );
 }
 
 export default App;
+
+// create a new component 
+
+function Profile({image, name, batch}){
+  return(
+    <div className='profile'>
+      <img src={image} alt={name}/>
+      <h2>{name}</h2>
+      <p>{batch}</p>
+    </div>
+  )
+}
+
+// const props = {
+//   image : "",
+//   name : "",
+//   batch : ""
+// }
+
+// const {image, name, batch} = props
+
+// function getname(name){
+//   return name 
+// }
+
+// getname("sanjay")
+  // return React.createElement(
+  //   'div',
+  //   {className : "profile-name", id:"profile-id"},
+  //   React.createElement("h2", null, "Profile name")
+  // )
